998d8dc91ab2d503e098d445115e17e8848fcd80
Benson Chau <bchau3440@gmail.com>
2018-07-17 17:17:35 -0400

    added clap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+extern crate gtk;
+#[macro_use]
+extern crate clap;
+
+use gtk::prelude::*;
+use clap::App;
+
 mod cpu;
 
 type BYTE = u8;
 type WORD = u16;
 
 fn main() {
-    
+    let yaml = load_yaml!("../res/config.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+
 }
