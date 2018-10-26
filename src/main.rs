e65eca9bdc54850c60f22fbc8894a0afcf19f836
Benson Chau <bchau3440@gmail.com>
2018-10-26 15:11:34 -0400

    added screen scaling to avoid 64x32 screen

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
-extern crate gtk;
 #[macro_use]
 extern crate clap;
+extern crate piston_window;
 
 use clap::App;
-use gtk::prelude::*;
-use gtk::{
-    Window, WindowType,
-};
+use piston_window::*;
 
 mod font;
 mod cpu;
@@ -17,15 +14,22 @@ type WORD = u16;
 fn main() {
     let yaml = load_yaml!("../res/config.yml");
     let matches = App::from_yaml(yaml).get_matches();
+    
+    // screen_scaling here means that every 'pixel' is screen_scaling x screen_scaling size.
+    let screen_scaling = 10;
+    let scr = [64 * screen_scaling, 32 * screen_scaling];
 
-    gtk::init().unwrap();
-    let window = Window::new(WindowType::Toplevel);
+    let mut window: PistonWindow = 
+        WindowSettings::new("rust-chip8", scr)
+        .exit_on_esc(true).build().unwrap();
 
-    window.show_all();
-    window.connect_delete_event(|_, _| {
-        gtk::main_quit();
-        Inhibit(false)
-    });
-
-    gtk::main();
+    while let Some(event) = window.next() {
+        window.draw_2d(&event, |context, graphics| {
+            clear([1.0; 4], graphics);
+            rectangle([1.0, 0.0, 0.0, 1.0],
+                      [0.0, 0.0, 100.0, 100.0],
+                      context.transform,
+                      graphics);
+        });
+    }
 }
