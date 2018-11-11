105ca9526eaa669257a2e4928a97eb41c29a68fc
Benson Chau <bchau3440@gmail.com>
2018-11-11 17:15:25 -0500

    moved graphics logic into screen.rs

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ extern crate opengl_graphics;
 
 mod font;
 mod cpu;
+mod screen;
 
 type BYTE = u8;
 type WORD = u16;
@@ -17,41 +18,6 @@ use piston::input::*;
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{GlGraphics, OpenGL};
 use std::path::Path;
-
-pub struct App {
-    gl: GlGraphics,
-    rotation: f64
-}
-impl App {
-    fn render(&mut self, args: &RenderArgs) {
-        use graphics::*;
-
-        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-
-        let square = rectangle::square(0.0, 0.0, 50.0);
-        let rotation = self.rotation;
-        let (x, y) = ((args.width / 2) as f64,
-                      (args.height / 2) as f64);
-
-        self.gl.draw(args.viewport(), |c, gl| {
-            // Clear the screen.
-            clear(GREEN, gl);
-
-            let transform = c.transform.trans(x, y)
-                                       .rot_rad(rotation)
-                                       .trans(-25.0, -25.0);
-
-            // Draw a box rotating around the middle of the screen.
-            rectangle(RED, square, transform, gl);
-        });
-    }
-
-    fn update(&mut self, args: &UpdateArgs) {
-        // Rotate 2 radians per second.
-        self.rotation += 2.0 * args.dt;
-    }
-}
 
 fn main() {
     let yaml = load_yaml!("../res/config.yml");
@@ -83,7 +49,7 @@ fn main() {
         .unwrap();
 
     // Create a new game and run it.
-    let mut app = App {
+    let mut app = screen::App {
         gl: GlGraphics::new(opengl),
         rotation: 0.0
     };
